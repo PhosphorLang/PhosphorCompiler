faf0559f81d491d5b57e6a9fa877a8f6c458bdd2
Benedikt Magnus <magnus@magnuscraft.de>
2020-03-05 19:45:00 +0100

    Main class with command line parameters

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+class Main
+{
+    private filePath: string;
+    private outputPath: string;
+
+    constructor ()
+    {
+        // TODO: Own class for command line parameters.
+
+        let indexOfFileArgument = process.argv.indexOf('-f');
+        if (indexOfFileArgument === -1)
+        {
+            indexOfFileArgument = process.argv.indexOf('--file');
+
+            if (indexOfFileArgument === -1)
+            {
+                throw new Error('No file given.');
+            }
+        }
+
+        let indexOfOutputArgument = process.argv.indexOf('-o');
+        if (indexOfOutputArgument === -1)
+        {
+            indexOfOutputArgument = process.argv.indexOf('--output');
+
+            if (indexOfOutputArgument === -1)
+            {
+                throw new Error('No output file path given.');
+            }
+        }
+
+        this.filePath = process.argv[indexOfFileArgument + 1];
+        this.outputPath = process.argv[indexOfOutputArgument + 1];
+    }
+
+    public run (): void
+    {
+        console.log('run: ' + this.filePath + ' - ' + this.outputPath);
+    }
+}
+
+const main = new Main();
+main.run();

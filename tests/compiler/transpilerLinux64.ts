f0adc11b3bffd9419241dc141cb059f0b9e1baca
Benedikt Magnus <magnus@magnuscraft.de>
2020-03-12 19:46:50 +0100

    Additional tests for the transpiler

--- tests/compiler/transpilerLinux64.ts
+++ tests/compiler/transpilerLinux64.ts
@@ -16,7 +16,8 @@ describe('TranspilerLinux64',
                 const input = new ActionTreeNode(null, new ActionToken(SemanticalType.File, fileName));
                 new ActionTreeNode(input, new ActionToken(SemanticalType.Function, 'print'));
 
-                const expectedResult = "[section .rodata]\n" +
+                const expectedResult =
+                    "[section .rodata]\n" +
                     "[section .text]\n" +
                     "[global _start]\n" +
                     "[extern print]\n" +
@@ -33,5 +34,67 @@ describe('TranspilerLinux64',
                 assert.deepStrictEqual(result, expectedResult);
             }
         );
+
+        it('can transpile an integer parameter.',
+            function ()
+            {
+                const input = new ActionTreeNode(null, new ActionToken(SemanticalType.File, fileName));
+                new ActionTreeNode(input, new ActionToken(SemanticalType.Function, 'print'));
+                new ActionTreeNode(input.children[0], new ActionToken(SemanticalType.IntegerLiteral, 'c_0', '24'));
+                new ActionTreeNode(input, new ActionToken(SemanticalType.IntegerDefinition, 'c_0', '24'));
+
+                const expectedResult =
+                    "[section .rodata]\n" +
+                    "c_0: db 24\n" +
+                    "[section .text]\n" +
+                    "[global _start]\n" +
+                    "[extern print]\n" +
+                    "_start:\n" +
+                    "mov rdi, c_0\n" +
+                    "mov rsi, 1\n" +
+                    "call print\n" +
+                    "mov rdi, 0\n" +
+                    "mov rax, 60\n" +
+                    "syscall\n";
+
+                const transpiler = new TranspilerLinux64();
+
+                const result = transpiler.run(input);
+
+                assert.deepStrictEqual(result, expectedResult);
+            }
+        );
+
+        it('can transpile a string parameter.',
+            function ()
+            {
+                const input = new ActionTreeNode(null, new ActionToken(SemanticalType.File, fileName));
+                new ActionTreeNode(input, new ActionToken(SemanticalType.Function, 'print'));
+                new ActionTreeNode(input.children[0], new ActionToken(SemanticalType.StringLiteral, 'c_0', 'Test string'));
+                new ActionTreeNode(input, new ActionToken(SemanticalType.StringDefinition, 'c_0', 'Test string'));
+
+                const expectedResult =
+                    "[section .rodata]\n" +
+                    "c_0: db 'Test string'\n" +
+                    "[section .text]\n" +
+                    "[global _start]\n" +
+                    "[extern print]\n" +
+                    "_start:\n" +
+                    "mov rdi, c_0\n" +
+                    "mov rsi, 11\n" +
+                    "call print\n" +
+                    "mov rdi, 0\n" +
+                    "mov rax, 60\n" +
+                    "syscall\n";
+
+                const transpiler = new TranspilerLinux64();
+
+                const result = transpiler.run(input);
+
+                assert.deepStrictEqual(result, expectedResult);
+            }
+        );
+
+        // TODO: Test UTF-8 strings in every stage.
     }
 );

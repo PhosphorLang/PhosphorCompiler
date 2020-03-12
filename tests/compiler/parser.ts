93ff88e2eb85abbec28592851a2d05f6c3ec08a8
Benedikt Magnus <magnus@magnuscraft.de>
2020-03-12 16:08:00 +0100

    Parameter tests for the parser

--- tests/compiler/parser.ts
+++ tests/compiler/parser.ts
@@ -34,6 +34,54 @@ describe('Parser',
             }
         );
 
+        it('can parse an integer parameter.',
+            function ()
+            {
+                const input = [
+                    new Token(LexicalType.File, fileName),
+                    new Token(LexicalType.Id, 'print'),
+                    new Token(LexicalType.Operator, Operator.openingBracket),
+                    new Token(LexicalType.Number, '24'),
+                    new Token(LexicalType.Operator, Operator.closingBracket),
+                    new Token(LexicalType.Operator, Operator.semicolon),
+                ];
+
+                const expectedResult = new SyntaxTreeNode(null, new Token(LexicalType.File, fileName));
+                new SyntaxTreeNode(expectedResult, new Token(LexicalType.Id, 'print'));
+                new SyntaxTreeNode(expectedResult.children[0], new Token(LexicalType.Number, '24'));
+
+                const parser = new Parser();
+
+                const result = parser.run(input);
+
+                assert.deepStrictEqual(result, expectedResult);
+            }
+        );
+
+        it('can parse a string parameter.',
+            function ()
+            {
+                const input = [
+                    new Token(LexicalType.File, fileName),
+                    new Token(LexicalType.Id, 'print'),
+                    new Token(LexicalType.Operator, Operator.openingBracket),
+                    new Token(LexicalType.String, 'Test string'),
+                    new Token(LexicalType.Operator, Operator.closingBracket),
+                    new Token(LexicalType.Operator, Operator.semicolon),
+                ];
+
+                const expectedResult = new SyntaxTreeNode(null, new Token(LexicalType.File, fileName));
+                new SyntaxTreeNode(expectedResult, new Token(LexicalType.Id, 'print'));
+                new SyntaxTreeNode(expectedResult.children[0], new Token(LexicalType.String, 'Test string'));
+
+                const parser = new Parser();
+
+                const result = parser.run(input);
+
+                assert.deepStrictEqual(result, expectedResult);
+            }
+        );
+
         it('throws an exception at unknown tokens.',
             function ()
             {

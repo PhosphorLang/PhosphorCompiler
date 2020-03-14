2f74bcf664fb295bbb7ed9d701fa84ea18ec41c5
Benedikt Magnus <magnus@magnuscraft.de>
2020-03-14 08:13:09 +0100

    Removed obsolete statement types.

--- src/parser/statementType.ts
+++
@@ -1,9 +0,0 @@
-enum StatementType
-{
-    None, // "value" or "valueA valueB" or ...
-    UnaryLeft, // "operator value"
-    UnaryRight, // "value operator"
-    Binary, // "valueA operator valueB"
-}
-
-export default StatementType;

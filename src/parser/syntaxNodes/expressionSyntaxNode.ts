525a85825976e3b052dbd0811ad39715f7bac548
Benedikt Magnus <magnus@magnuscraft.de>
2020-03-27 14:36:30 +0100

    ESLint: ban-ts-ignore is now a warning instead of an error.

--- src/parser/syntaxNodes/expressionSyntaxNode.ts
+++ src/parser/syntaxNodes/expressionSyntaxNode.ts
@@ -2,7 +2,6 @@ import SyntaxNode from "./syntaxNode";
 
 export default abstract class ExpressionSyntaxNode extends SyntaxNode
 {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore Workaround to enable static typing for this class.
     private staticTyping = true;
 }

1dc0d9443ad672e6398d0184de4d700263d8e95a
Benedikt Magnus <magnus@magnuscraft.de>
2020-03-29 20:24:00 +0200

    Build in functions

---
+++ src/definitions/buildInFunctions.ts
@@ -0,0 +1,14 @@
+import BuildInTypes from "./buildInTypes";
+import FunctionSemanticSymbol from "../connector/semanticSymbols/functionSemanticSymbol";
+import ParameterSemanticSymbol from "../connector/semanticSymbols/parameterSemanticSymbol";
+
+export default abstract class BuildInFunctions
+{
+    // TODO: In the long run, this will be replaced by the Standard Library. It will contain definitions for these functions.
+
+    public static readonly print = new FunctionSemanticSymbol(
+        'print',
+        BuildInTypes.noType,
+        [new ParameterSemanticSymbol('text', BuildInTypes.string)]
+    );
+}

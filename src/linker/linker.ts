5436fa17050f78b5a363e8b7120379e932a1c7c5
Benedikt Magnus <magnus@magnuscraft.de>
2020-05-05 18:52:00 +0200

    Set linker's starting point explicitly to "_start".

--- src/linker/linker.ts
+++ src/linker/linker.ts
@@ -6,6 +6,6 @@ export default class Linker
     {
         const filesAsString = files.join('" "'); // TODO: Give a better name.
 
-        childProcess.execSync('ld -s --gc-sections -o "' + outputPath + '" "' + filesAsString + '"');
+        childProcess.execSync('ld -e _start -s --gc-sections -o "' + outputPath + '" "' + filesAsString + '"');
     }
 }

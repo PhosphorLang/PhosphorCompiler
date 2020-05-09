5f636726f5f3e5270cb38d4b92e8c10a607f7d42
Benedikt Magnus <magnus@magnuscraft.de>
2020-05-09 12:47:02 +0200

    Replaced the action tree builder in the test utility with a semantic creator.
    Now it fits the new connector structure.

---
+++ tests/utility/semanticCreator.ts
@@ -0,0 +1,84 @@
+import * as SemanticNodes from "../../src/connector/semanticNodes";
+import * as SemanticSymbols from "../../src/connector/semanticSymbols";
+import BinarySemanticOperator from "../../src/connector/semanticOperators/binarySemanticOperator";
+import BuildInOperators from "../../src/definitions/buildInOperators";
+import BuildInTypes from "../../src/definitions/buildInTypes";
+import Defaults from "./defaults";
+
+export default abstract class SemanticCreator
+{
+    public static newFile (functions: SemanticNodes.Function[] = [], fileName = Defaults.fileName): SemanticNodes.File
+    {
+        return new SemanticNodes.File(fileName, functions);
+    }
+
+    public static newFunctionDeclaration (section = SemanticCreator.newSection(), symbol = SemanticCreator.newFunctionSymbol()): SemanticNodes.Function
+    {
+        return new SemanticNodes.Function(symbol, section);
+    }
+
+    public static newSection (statements: SemanticNodes.SemanticNode[] = []): SemanticNodes.Section
+    {
+        return new SemanticNodes.Section(statements);
+    }
+
+    public static newFunctionSymbol (
+        parameters: SemanticSymbols.Parameter[] = [],
+        returnType = BuildInTypes.noType,
+        name = Defaults.identifier
+    ): SemanticSymbols.Function
+    {
+        return new SemanticSymbols.Function(name, returnType, parameters);
+    }
+
+    public static newFunctionCall (
+        callArguments: SemanticNodes.Expression[] = [],
+        symbol = SemanticCreator.newFunctionSymbol()
+    ): SemanticNodes.CallExpression
+    {
+        return new SemanticNodes.CallExpression(symbol, callArguments);
+    }
+
+    public static newVariableDeclaration (
+        initialiser: SemanticNodes.Expression|null = null,
+        symbol = SemanticCreator.newVariableSymbol()
+    ): SemanticNodes.VariableDeclaration
+    {
+        return new SemanticNodes.VariableDeclaration(symbol, initialiser);
+    }
+
+    public static newVariableSymbol (
+        type = BuildInTypes.int,
+        name = Defaults.variableName,
+        isReadonly = false
+    ): SemanticSymbols.Variable
+    {
+        return new SemanticSymbols.Variable(name, type, isReadonly);
+    }
+
+    public static newAssignment (expression: SemanticNodes.Expression, variable = SemanticCreator.newVariableSymbol()): SemanticNodes.Assignment
+    {
+        return new SemanticNodes.Assignment(variable, expression);
+    }
+
+    public static newIntegerLiteral (value = Defaults.integer): SemanticNodes.LiteralExpression
+    {
+        return new SemanticNodes.LiteralExpression(value, BuildInTypes.int);
+    }
+
+    public static newBinaryExpression (
+        left: SemanticNodes.Expression,
+        operator: BinarySemanticOperator,
+        right: SemanticNodes.Expression): SemanticNodes.BinaryExpression
+    {
+        return new SemanticNodes.BinaryExpression(operator, left, right);
+    }
+
+    public static newIntegerAddition (
+        left = SemanticCreator.newIntegerLiteral(),
+        right = SemanticCreator.newIntegerLiteral()
+    ): SemanticNodes.BinaryExpression
+    {
+        return SemanticCreator.newBinaryExpression(left, BuildInOperators.binaryIntAddition, right);
+    }
+}

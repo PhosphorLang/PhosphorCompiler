bb7917f00f4c527603de7b3b4588780877a92b13
Benedikt Magnus <magnus@magnuscraft.de>
2020-05-10 10:27:22 +0200

    Fixed that a colon would swallow the next char.

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -109,6 +109,8 @@ export default class Lexer
                 }
                 else
                 {
+                    this.position--;
+
                     kind = TokenKind.ColonToken;
                 }
                 break;

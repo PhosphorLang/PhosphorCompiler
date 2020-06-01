b91abf7a610401e821df8d8c54b3919495392a04
Benedikt Magnus <magnus@magnuscraft.de>
2020-06-01 18:10:09 +0200

    Added new string build in functions "stringToInt" and "intToString".

--- src/definitions/buildInFunctions.ts
+++ src/definitions/buildInFunctions.ts
@@ -12,10 +12,23 @@ export default abstract class BuildInFunctions
         [new ParameterSemanticSymbol('text', BuildInTypes.string)]
     );
 
+    public static readonly stringToInt = new FunctionSemanticSymbol(
+        'stringToInt',
+        BuildInTypes.int,
+        [new ParameterSemanticSymbol('string', BuildInTypes.string)]
+    );
+    public static readonly intToString = new FunctionSemanticSymbol(
+        'intToString',
+        BuildInTypes.string,
+        [new ParameterSemanticSymbol('integer', BuildInTypes.int)]
+    );
+
     public static readonly readLine = new FunctionSemanticSymbol('readLine', BuildInTypes.string, []);
 
     public static functions: FunctionSemanticSymbol[] = [
         BuildInFunctions.writeLine,
         BuildInFunctions.readLine,
+        BuildInFunctions.stringToInt,
+        BuildInFunctions.intToString,
     ];
 }

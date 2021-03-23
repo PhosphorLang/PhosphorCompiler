1ba05f9cb906bd90749981adc6acb5075d5543a6
Benedikt Magnus <magnus@magnuscraft.de>
2021-03-23 21:22:49 +0100

    AVR linker

---
+++ src/linker/avr/linkerAvr.ts
@@ -0,0 +1,18 @@
+import childProcess from 'child_process';
+import Linker from '../linker';
+
+export default class LinkerAvr implements Linker
+{
+    public run (outputPath: string, files: string[]): void
+    {
+        const filesAsString = files.join('" "'); // TODO: Give a better name.
+
+        childProcess.execSync(
+            'avr-ld ' +
+            '-s --gc-sections ' +
+            '-nostdlib -nolibc ' +
+            '-o "' + outputPath + '" ' +
+            '"' + filesAsString + '"'
+        );
+    }
+}

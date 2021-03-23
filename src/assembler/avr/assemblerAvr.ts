51dca1a86431ee6a2d249d125fe526d8a238a988
Benedikt Magnus <magnus@magnuscraft.de>
2021-03-23 21:22:23 +0100

    AVR Assembler

---
+++ src/assembler/avr/assemblerAvr.ts
@@ -0,0 +1,10 @@
+import Assembler from '../assembler';
+import childProcess from 'child_process';
+
+export default class AssemblerAvr implements Assembler
+{
+    public run (assemblyPath: string, outputPath: string): void
+    {
+        childProcess.execSync(`avr-as -o "${outputPath}" ${assemblyPath}`);
+    }
+}

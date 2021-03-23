ff5073f09a169079e5c04e4ee35ac0575a077930
Benedikt Magnus <magnus@magnuscraft.de>
2021-03-23 21:18:21 +0100

    Simplified AVR registers (for now)

--- src/transpiler/avr/registersAvr.ts
+++ src/transpiler/avr/registersAvr.ts
@@ -3,6 +3,7 @@ import Register8 from "../common/registers/register8";
 
 export default abstract class RegistersAvr
 {
+    // General purpose registers:
     public static readonly r0: Register8 = new Register8('r0');
     public static readonly r1: Register8 = new Register8('r1');
     public static readonly r2: Register8 = new Register8('r2');
@@ -36,10 +37,90 @@ export default abstract class RegistersAvr
     public static readonly r30: Register8 = new Register8('r30');
     public static readonly r31: Register8 = new Register8('r31');
 
+    // Pointer registers:
     public static readonly x: Register16 = new Register16('r27:r26', 'r26');
     public static readonly y: Register16 = new Register16('r29:r28', 'r28');
     public static readonly z: Register16 = new Register16('r31:r30', 'r30');
 
+    // Special registers:
     public static readonly sreg: Register8 = new Register8('sreg');
     public static readonly sp: Register16 = new Register16('sph:spl', 'spl');
+
+    // Register classes
+
+    public static readonly callerSaved: Register8[] = [];
+
+    public static readonly calleeSaved: Register8[] = [
+        RegistersAvr.r1,
+        RegistersAvr.r2,
+        RegistersAvr.r3,
+        RegistersAvr.r4,
+        RegistersAvr.r5,
+        RegistersAvr.r6,
+        RegistersAvr.r7,
+        RegistersAvr.r8,
+        RegistersAvr.r9,
+        RegistersAvr.r10,
+        RegistersAvr.r11,
+        RegistersAvr.r12,
+        RegistersAvr.r13,
+        RegistersAvr.r14,
+        RegistersAvr.r15,
+        RegistersAvr.r16,
+        RegistersAvr.r17,
+        RegistersAvr.r18,
+        RegistersAvr.r19,
+        RegistersAvr.r20,
+        RegistersAvr.r21,
+        RegistersAvr.r22,
+        RegistersAvr.r23,
+        RegistersAvr.r24,
+        RegistersAvr.r25,
+        RegistersAvr.r26,
+        RegistersAvr.r27,
+        RegistersAvr.r28,
+        RegistersAvr.r29,
+        RegistersAvr.r30,
+        RegistersAvr.r31,
+    ];
+
+    public static readonly argumentValues: Register8[] = [
+        RegistersAvr.r25,
+        RegistersAvr.r24,
+        RegistersAvr.r23,
+        RegistersAvr.r22,
+        RegistersAvr.r21,
+        RegistersAvr.r20,
+        RegistersAvr.r19,
+        RegistersAvr.r18,
+        RegistersAvr.r17,
+        RegistersAvr.r16,
+        RegistersAvr.r15,
+        RegistersAvr.r14,
+        RegistersAvr.r13,
+        RegistersAvr.r12,
+        RegistersAvr.r11,
+        RegistersAvr.r10,
+    ];
+
+    public static readonly returnValue: Register8[] = [
+        RegistersAvr.r25,
+        RegistersAvr.r24,
+        RegistersAvr.r23,
+        RegistersAvr.r22,
+        RegistersAvr.r21,
+        RegistersAvr.r20,
+        RegistersAvr.r19,
+        RegistersAvr.r18,
+    ];
+
+    public static readonly pointers: Register16[] = [
+        RegistersAvr.x,
+        RegistersAvr.y,
+        RegistersAvr.z,
+    ];
+
+    public static readonly zero: Register8 = RegistersAvr.r0;
+
+    public static readonly stackPointer: Register16 = RegistersAvr.sp;
 }

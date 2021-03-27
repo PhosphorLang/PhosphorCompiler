e1853bdbec08bb6d2f2ccd87738f3cc04bca8079
Benedikt Magnus <magnus@magnuscraft.de>
2021-03-27 23:49:16 +0100

    LocationedVariableAvr

---
+++ src/transpiler/avr/locationedVariableAvr.ts
@@ -0,0 +1,19 @@
+import * as SemanticSymbols from "../../connector/semanticSymbols";
+import RegisterAvr from "./registers/registerAvr";
+
+export default class LocationedVariableAvr
+{
+    public readonly variable: SemanticSymbols.Variable;
+    public location: RegisterAvr[];
+
+    public get size (): number
+    {
+        return this.location.length;
+    }
+
+    constructor (variable: SemanticSymbols.Variable, location: RegisterAvr[])
+    {
+        this.variable = variable;
+        this.location = location;
+    }
+}

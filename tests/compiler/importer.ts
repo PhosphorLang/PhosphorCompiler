a6249d5f240dbd6b1710f460ad7acbf2c3cc3473
Benedikt Magnus <magnus@magnuscraft.de>
2022-02-24 21:18:25 +0100

    The importer test is now more generic.
    Instead of using "/dev/null" it uses the file name of the importer test source code. This should work in all platforms (including the ones missing "/dev/null").

--- tests/compiler/importer.ts
+++ tests/compiler/importer.ts
@@ -4,6 +4,7 @@ import Diagnostic from '../../src/diagnostic/diagnostic';
 import Importer from '../../src/importer/importer';
 import Lexer from '../../src/lexer/lexer';
 import Parser from '../../src/parser/parser';
+import path from 'path';
 import Sinon from 'sinon';
 import SyntaxCreator from '../utility/syntaxCreator';
 import TokenCreator from '../utility/tokenCreator';
@@ -34,8 +35,10 @@ describe('Importer',
         it('can import a single empty file.',
             function ()
             {
+                const filePathStub = __filename;
+
                 const importSyntaxNode = SyntaxCreator.newImport(
-                    TokenCreator.newString('/null')
+                    TokenCreator.newString('/' + path.basename(filePathStub))
                 );
 
                 const input = SyntaxCreator.newFile(
@@ -47,12 +50,7 @@ describe('Importer',
 
                 parserStub.run.returns(importedFile);
 
-                // NOTE: Using "/dev/null" is two hacks in one:
-                // 1. It doesn't matter what the file path is, as long as it is a valid file. Thus /dev/null is enough.
-                //    The reason for this is that the importer will read the file but the lexer and parser stub will return fixed values
-                //    no matter the given file contents.
-                // 2. The function expects a FILE path, not a directory, thus the double "null", once here and another one in the import.
-                const result = importer.run(input, '/dev/null');
+                const result = importer.run(input, filePathStub);
 
                 assert.hasAllDeepKeys(
                     result,

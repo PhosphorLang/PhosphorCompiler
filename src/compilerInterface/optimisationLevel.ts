7c58436f72637c5a855948bd2bf251f96a074bfb
Benedikt Magnus <magnus@magnuscraft.de>
2025-12-30 15:49:38 +0100

    Smaller things and comments

--- src/compilerInterface/optimisationLevel.ts
+++ src/compilerInterface/optimisationLevel.ts
@@ -2,10 +2,11 @@
  * The possible optimisation levels for the compiler.
  * NOTE: You can assume that the values are of type string and thus strings are castable to this enum.
  */
- export enum OptimisationLevel
+export enum OptimisationLevel
 {
     None = 'none',
     Balanced = 'balanced',
     Performance = 'performance',
     Size = 'size',
 }
+// TODO: The optimisation level should be replaced or extended with optimisation flags than enable/disable certain optimisations.
